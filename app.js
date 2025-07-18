8cc24e81b6c95d285b0ce0c20207bcd41a5437fe
Francisco Sena <franciscomoreira0202@gmail.com>
2025-07-18 15:19:43 -0300

    Adicionando histórico

--- app.js
+++ app.js
@@ -8,13 +8,28 @@ app.use(require('cors')());
 // 1. ROTAS DA API PRIMEIRO
 
 let dados = { nivel: 0, status: "OK" };
+let historico = []; //array
 
 app.get('/status', (req, res) => res.json(dados));
 
 app.post('/update', (req, res) => {
-  dados = req.body;
+  dados = req.body; //req.body = dados enviados no corpo da requisição (geralmente JSON).
+
+  historico.unshift({ //unshift, insere o novo dado no início da lista (posição 0).O histórico é uma array
+    nivel: dados.nivel, //nivel: número recebido.
+    status: dados.status, //status: texto atual.
+    timestamp: new Date().toISOString()//timestamp: horário da atualização (formato ISO, como 2025-07-18T14:30:00.000Z).
+  });
+  if (historico.length > 15) {
+    historico.pop();
+  }
+
   console.log('Dados atualizados:', dados);
   res.send('Atualizado');
+});
+
+app.get('/historico', (req, res) => {
+  res.json(historico);
 });
 
 // 2. ARQUIVOS ESTÁTICOS DEPOIS
