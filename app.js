2bbb0f8b3d42722d9eb70ef0bc435fff38eb7846
Francisco Sena <franciscomoreira0202@gmail.com>
2025-07-22 16:58:38 -0300

    Commit

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ mongoose.connect('mongodb+srv://franciscomoreira0202:JX8BOG9nzNQe9KkM@cluster0.u
 const Historico = mongoose.model('Historico', new mongoose.Schema({
   nivel: Number,
   status: String,
-  timestamp: { type: Date, default: formatarDataHora }
+  timestamp: { type: Date, default: Date.now }
 }));
 
 app.use(express.json());
